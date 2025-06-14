d6af6da4bb0461f7be2b5dc6e829b7b847098f7f
Sameer Katija <sameerkatija@gmail.com>
2025-06-14 22:53:45 +0500

    added animation

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ tl.from("#brand-title", {
   y: -30,
   opacity: 0,
   duration: 0.67,
-  delay: 0.2,
 });
 
 tl.from("#brand-nav a", {
@@ -14,28 +13,44 @@ tl.from("#brand-nav a", {
   stagger: 0.1,
 });
 
-const t2 = gsap.timeline();
-
-t2.from("#hero-heading", {
+gsap.from("#hero-heading", {
   x: -100,
   opacity: 0,
-  delay: 0.2,
   duration: 0.44,
-  stagger: 0.5,
+  stagger: 0.2,
 });
 
-t2.from("#hero-img", {
+gsap.from("#hero-img", {
   x: -100,
   y: 200,
   duration: 0.4,
-  delay: 0.5,
   opacity: 0,
   rotate: 180,
+  scrollTrigger: "#hero-imgs",
 });
 
-t2.from("#form", {
+gsap.from("#form", {
   x: 100,
   duration: 0.2,
-  delay: 2,
   opacity: 0,
+  scrollTrigger: {
+    trigger: "#form",
+    scroller: "body",
+    // scrub: true,
+    start: "top 50%",
+    // markers: true,
+  },
+});
+
+gsap.to("#about-id", {
+  transform: "translateX(-150%)",
+  scrollTrigger: {
+    trigger: "#about",
+    scroller: "body",
+    scrub: 2,
+    start: "top 0%",
+    end: "top -150%",
+    markers: true,
+    pin: true,
+  },
 });
